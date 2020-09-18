a475743af71c7a7bddefcd673f1b0198b85d3cb8
Egor <orfas@live.com>
2020-09-19 01:03:14 +0300

    first commit landing

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+$('.variable-width').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  centerMode: true,
+  variableWidth: true
+
+});
+	
+});
